82616ea7164923d262f2a0a98a0dad0069337997
Charles M Berg <77517285+CharlesBerg85@users.noreply.github.com>
2022-03-13 21:50:26 -0700

    Added Climbing fuctionality and sprites to climb

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,14 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
     [SerializeField] float jumpSpeed = 15f;
+    [SerializeField] float climbingSpeed = 8f; 
 
     Rigidbody2D myRigidbody2D;
     Animator myAnimator;
     BoxCollider2D myBoxCollider2D;
     PolygonCollider2D myPlayersFeet;
+
+    float startingGravityScale;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +23,8 @@ public class Player : MonoBehaviour
         myAnimator = GetComponent<Animator>();
         myBoxCollider2D = GetComponent<BoxCollider2D>();
         myPlayersFeet = GetComponent<PolygonCollider2D>();
+
+        startingGravityScale = myRigidbody2D.gravityScale;
        
     }
 
@@ -28,6 +33,30 @@ public class Player : MonoBehaviour
     {
         Run();
         Jump();
+        Climb();
+    }
+
+    private void Climb()
+    {
+
+        if (myBoxCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+            Vector2 climbingVelocity = new Vector2(myRigidbody2D.velocity.x, controlThrow * climbingSpeed);
+
+            myRigidbody2D.velocity = climbingVelocity;
+
+            myAnimator.SetBool("Climbing", true);
+
+            myRigidbody2D.gravityScale = 0f;
+        }
+        else
+        {
+            myAnimator.SetBool("Climbing", false);
+            myRigidbody2D.gravityScale = startingGravityScale;
+        }
+
+        
     }
 
     private void Jump()
