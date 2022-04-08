cce7d655c1d656a088886641ff6ca5215971d401
Charles M Berg <77517285+CharlesBerg85@users.noreply.github.com>
2022-04-07 23:06:29 -0700

    Heart & Diamond SFX added

--- Assets/Scripts/DiamondPickUp.cs
+++ Assets/Scripts/DiamondPickUp.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class DiamondPickUp : MonoBehaviour
 {
+    [SerializeField] AudioClip diamondPickupsSFX;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(diamondPickupsSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/HeartPickUp.cs
+++ Assets/Scripts/HeartPickUp.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class HeartPickUp : MonoBehaviour
 {
+    [SerializeField] AudioClip heartPickupsSFX;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(heartPickupsSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
